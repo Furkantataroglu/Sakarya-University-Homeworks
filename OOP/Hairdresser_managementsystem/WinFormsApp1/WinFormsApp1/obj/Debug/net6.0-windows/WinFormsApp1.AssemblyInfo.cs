6f73a9ac17ef16c3e3a74866332001820df2fde9
furkanttrgl <104869273+Furkantataroglu@users.noreply.github.com>
2024-05-19 03:05:44 +0300

    a

--- OOP/Hairdresser_managementsystem/WinFormsApp1/WinFormsApp1/obj/Debug/net6.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ OOP/Hairdresser_managementsystem/WinFormsApp1/WinFormsApp1/obj/Debug/net6.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40b0d26ef293fa270ba679070b2d2ad2dd8acd5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
